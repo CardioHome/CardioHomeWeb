58fc431689f43fc505cd579b37bcfb0f81485be0
Simon <zhangy405@gmail.com>
2018-03-08 15:16:21 -0800

    remove remote

--- CardioHomeWeb/urls.py
+++ CardioHomeWeb/urls.py
@@ -21,7 +21,7 @@ from www.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^login', login),
-
-    url(r'', home),
+    # url(r'^login', login),
+    #
+    # url(r'', home),
 ]
