46a16dbbdfcc24eedc5ce95f474545d584b2e8ab
Simon <zhangy405@gmail.com>
2018-03-08 15:14:38 -0800

    remove remote

--- www/views.py
+++ www/views.py
@@ -3,12 +3,12 @@ from django.http.response import HttpResponse
 
 import remote_auth
 
-# Create your views here.
-def home(request):
-    result = remote_auth.login('11', '456')
-    return HttpResponse('<h1>result %d</h1>' % result)
-
-
-def login(request):
-    result = remote_auth.login('112121212', '456')
-    return HttpResponse('result %d' % result)
+# # Create your views here.
+# def home(request):
+#     result = remote_auth.login('11', '456')
+#     return HttpResponse('<h1>result %d</h1>' % result)
+#
+#
+# def login(request):
+#     result = remote_auth.login('112121212', '456')
+#     return HttpResponse('result %d' % result)
